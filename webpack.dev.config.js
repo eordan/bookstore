8ea7997547ef468216198756dc32f0d1fa11246b
eordan <pistsouandrey@gmail.com>
2023-08-07 00:58:54 +0300

    feat: install and configure dotenv

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,13 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const dotenv = require('dotenv');
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
 
 module.exports = {
   mode: 'development',
@@ -17,5 +24,6 @@ module.exports = {
       filename: '[name].css',
       chunkFilename: '[id].css',
     }),
+    new webpack.DefinePlugin(envKeys),
   ],
 };
