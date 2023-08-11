cab25a629444d8a06b060fdaa1551441746b71ef
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-08-11 15:11:44 +0300

    feat: add simple footer

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import AppRouter from '@components/AppRouter';
 import Header from '@containers/Header';
+import Footer from '@containers/Footer';
 
 export function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Header />
       <AppRouter />
+      <Footer />
     </BrowserRouter>
   );
 }
