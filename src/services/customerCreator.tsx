ab0824a0a2de635ef92a86b656e23ebac45b38bb
eordan <pistsouandrey@gmail.com>
2023-08-12 13:41:13 +0300

    feat: update registration functionality

--- src/services/customerCreator.tsx
+++ src/services/customerCreator.tsx
@@ -1,11 +1,96 @@
 import { apiRoot } from './clientBuilder';
-import { CustomerDraft } from '../../utils/types';
+import { CustomerDraft, BaseAddress, EmailCheck } from '../../utils/types';
+
+export function checkEmailAndReturnInfo(customerEmail: string) {
+  const data = apiRoot
+    .customers()
+    .get({
+      queryArgs: {
+        where: `email="${customerEmail}"`,
+      },
+    })
+    .execute()
+    .then(({ body }) => {
+      if (body.results.length === 0) {
+        const emailCheck: EmailCheck = {
+          emailDoesExist: false,
+          message: 'This email address has not been registered.',
+        };
+        return emailCheck;
+      }
+      const emailCheck: EmailCheck = {
+        emailDoesExist: true,
+        message: 'This email address has already been registered.',
+      };
+      return emailCheck;
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+  return data;
+}
+
+export function transformUserInputToCustomerDetails(
+  email: string,
+  password: string,
+  firstName: string,
+  lastName: string,
+  dateOfBirth: string,
+  billingAndShippingAreSame: boolean,
+  shippingAddress: BaseAddress,
+  shippingIsDeafult: boolean,
+  billingAddress: BaseAddress,
+  billingIsDeafult: boolean,
+): CustomerDraft {
+  const addresses: BaseAddress[] = (() => {
+    if (billingAndShippingAreSame === true) {
+      return [shippingAddress];
+    }
+    return [shippingAddress, billingAddress];
+  })();
+
+  const billingAddresses: number[] = (() => {
+    if (billingAndShippingAreSame === true) {
+      return [0];
+    }
+    return [1];
+  })();
+
+  const customerDetails: CustomerDraft = {
+    email,
+    password,
+    firstName,
+    lastName,
+    dateOfBirth,
+    addresses,
+    shippingAddresses: [0],
+    billingAddresses,
+  };
+
+  if (shippingIsDeafult === true) {
+    customerDetails.defaultShippingAddress = 0;
+  }
+
+  if (billingIsDeafult === true) {
+    const [defaultBillingAddress] = customerDetails.billingAddresses;
+    customerDetails.defaultBillingAddress = defaultBillingAddress;
+  }
+
+  return customerDetails;
+}
 
 export function createCustomer(customerDetails: CustomerDraft) {
-  return apiRoot
+  apiRoot
     .customers()
     .post({
       body: customerDetails,
     })
-    .execute();
+    .execute()
+    .then(({ body }) => {
+      return body.customer.id;
+    })
+    .catch((error) => {
+      throw error;
+    });
 }
--- utils/types.tsx
+++ utils/types.tsx
@@ -13,4 +13,22 @@ export interface CustomerDraft {
   password: string;
   firstName: string;
   lastName: string;
+  dateOfBirth: string;
+  addresses: BaseAddress[];
+  defaultShippingAddress?: number;
+  shippingAddresses: number[];
+  defaultBillingAddress?: number;
+  billingAddresses: number[];
+}
+
+export interface BaseAddress {
+  country: string;
+  streetName: string;
+  postalCode: string;
+  city: string;
+}
+
+export interface EmailCheck {
+  emailDoesExist: boolean;
+  message: string;
 }
