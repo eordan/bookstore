a11191d7aa58cc45d589662da4b3dec5ccf775bb
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-08-15 16:28:25 +0300

    feat: make it possible to send data in login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Container, Form } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { RoutesEnum } from '../../utils/enums';
@@ -6,22 +6,35 @@ import { RoutesEnum } from '../../utils/enums';
 import './Login.scss';
 
 export function Login(): JSX.Element {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const signIn = () => {
+    // eslint-disable-next-line no-console
+    console.log(`Function, that send data to api\n ${email}, ${password}`);
+  };
+
   return (
     <Container className="d-flex flex-column justify-content-center align-items-center login-container">
       <h2>Welcome Back</h2>
       <Form className="d-flex flex-column mt-4">
         <Form.Group className="mt-3">
           <Form.Label>Email *</Form.Label>
-          <Form.Control placeholder="Enter your email" />
+          <Form.Control placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
         <Form.Group className="mt-3">
           <Form.Label>Password *</Form.Label>
-          <Form.Control placeholder="Enter your password" type="password" />
+          <Form.Control
+            placeholder="Enter your password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
-        <Button className="mt-3" variant="success">
+        <Button className="mt-3" variant="success" onClick={signIn}>
           Get started
         </Button>
-        <div className="d-flex justify-content-evenly mt-3">
+        <div className="d-flex justify-content-evenly mt-3 mb-5">
           <p>Already have an account?</p>
           <NavLink className="sign-up pl-3" to={RoutesEnum.REGISTRATION_ROUTE}>
             Sign Up
