a070ad4499917243cc96d0ed6a0ed70845849037
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-08-21 02:01:18 +0300

    fix: delete comments

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -9,25 +9,6 @@ import './Footer.scss';
 import github from '../../assets/github.png';
 
 export function Footer(): JSX.Element {
-  // return (
-  //   <footer className="footer">
-  //     <Container className="d-flex justify-content-between p-4">
-  //       <h2>Liblion</h2>
-  //       <Navbar.Collapse id="responsive-navbar-nav">
-  //         <Nav className="me-auto">
-  //           {LINKS_ARRAY.map((link) => (
-  //             <Links key={link.id} to={link.to} title={link.title} />
-  //           ))}
-  //         </Nav>
-  //       </Navbar.Collapse>
-  //       <div>
-  //         <NavLink target="_blank" to="https://github.com/eordan/bookstore/tree/develop">
-  //           <img src={github} alt="github" />
-  //         </NavLink>
-  //       </div>
-  //     </Container>
-  //   </footer>
-  // );
   return (
     <Navbar className="p-3 footer" data-bs-theme="dark">
       <Container>
