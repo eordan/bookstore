3b960cee90bbbd0b90b54c2561aa528b41668198
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-08-21 22:52:39 +0300

    fix: remove the error in the console

--- src/components/Links/Links.tsx
+++ src/components/Links/Links.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
 
 interface Props {
   to: string;
@@ -8,11 +7,5 @@ interface Props {
 }
 
 export function Links({ to, title }: Props): JSX.Element {
-  return (
-    <Nav.Link href="#">
-      <NavLink style={{ textDecoration: 'none', color: 'inherit' }} to={to}>
-        {title}
-      </NavLink>
-    </Nav.Link>
-  );
+  return <Nav.Link href={to}>{title}</Nav.Link>;
 }
