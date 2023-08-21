b3e433170333ca66c83bd3c89a8ea5ad0568a320
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-08-21 03:00:22 +0300

    fix: style in OopsDiv

--- src/components/OopsDiv/OopsDiv.tsx
+++ src/components/OopsDiv/OopsDiv.tsx
@@ -3,17 +3,19 @@ import { Button, Container } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { RoutesEnum } from '../../utils/enums';
 
+import './OopsDiv.scss';
+
 import robot from '../../assets/robot.svg';
 
 export function OopsDiv(): JSX.Element {
   const navigate = useNavigate();
 
   return (
-    <div className="p-5 mt-5 bg-light">
-      <Container className="d-flex justify-content-center flex-column align-items-center">
+    <div className="p-5 bg-light">
+      <Container className="d-flex justify-content-center flex-column align-items-center text-center">
         <img src={robot} alt="robot" />
         <h3 className="m-3">Oops...</h3>
-        <p>We can&apos;t seem to find the page you&apos;re looking for</p>
+        <p className="oopsDiv-subtitle">We can&apos;t seem to find the page you&apos;re looking for</p>
         <Button variant="success" onClick={() => navigate(RoutesEnum.MAIN_ROUTE)}>
           Back to Home
         </Button>
