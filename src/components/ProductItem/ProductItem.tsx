41494e57aaeb7f842cb412c83d3c4b6335ead382
Alina <lina.radyukevich98@gmail.com>
2023-08-27 17:47:19 +0300

    feat: implement simple products rendering structure

---
+++ src/components/ProductItem/ProductItem.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function ProductItem(): JSX.Element {
+  return (
+    <div>
+      <h1>Product List</h1>
+    </div>
+  );
+}
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,5 +1,6 @@
+import ProductList from '@components/ProductList';
 import React from 'react';
 
 export function Products(): JSX.Element {
-  return <h1 className="align-self-center">Welcome to the Products page</h1>;
+  return <ProductList />;
 }
