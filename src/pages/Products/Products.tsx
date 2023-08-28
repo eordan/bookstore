55d8a319086594c0d5a4f65c6b7ab77f8dbb0109
Alina <lina.radyukevich98@gmail.com>
2023-08-28 22:31:56 +0300

    feat: add simple search & sort fields

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,52 +1,24 @@
 import ProductList from '@components/ProductList';
-import React, { useState } from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import Filters from '@containers/Filters';
+import React from 'react';
+import { Button, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap';
 
 export function Products(): JSX.Element {
-  const [isPriceSort, setIsPriceSort] = useState(false);
-  const [priceSort, setPriceSort] = useState('&darr;');
-  const [isAlphabeticalSort, setIsAlphabeticalSort] = useState(false);
-  const [alphabeticalSort, setAlphabeticalSort] = useState('&darr;');
-
   return (
     <section className=" mt-3 bg-light">
       <Container>
         <Form className="d-flex justify-content-end pt-4">
           <Row>
-            <Col className="d-flex justify-content-around align-items-center">
-              <Form.Check
-                type="checkbox"
-                checked={isPriceSort}
-                onChange={(e) => setIsPriceSort(e.target.checked)}
-                label="Sort by price?"
-              />
-              {isPriceSort && (
-                <Button
-                  className="sort-btn"
-                  dangerouslySetInnerHTML={{ __html: priceSort }}
-                  onClick={() => {
-                    if (priceSort === '&darr;') setPriceSort('&uarr;');
-                    else setPriceSort('&darr;');
-                  }}
-                />
-              )}
-              <Form.Check
-                type="checkbox"
-                checked={isAlphabeticalSort}
-                onChange={(e) => setIsAlphabeticalSort(e.target.checked)}
-                label="Sort alphabetically?"
-              />
-              {isAlphabeticalSort && (
-                <Button
-                  dangerouslySetInnerHTML={{ __html: alphabeticalSort }}
-                  onClick={() => {
-                    if (alphabeticalSort === '&darr;') setAlphabeticalSort('&uarr;');
-                    else setAlphabeticalSort('&darr;');
-                  }}
-                />
-              )}
+            <Col>
+              <FloatingLabel label="Arrange">
+                <Form.Select>
+                  <option value="1">Cheap ones first</option>
+                  <option value="2">Expensive ones first</option>
+                  <option value="3">Alphabetically</option>
+                </Form.Select>
+              </FloatingLabel>
             </Col>
-            <Col className="d-flex">
+            <Col className="d-flex align-items-center">
               <Form.Control type="search" placeholder="Search" className="me-2" />
               <Button variant="outline-success">Search</Button>
             </Col>
@@ -54,7 +26,7 @@ export function Products(): JSX.Element {
         </Form>
         <Row className="mt-4">
           <Col md={3}>
-            <Form.Control />
+            <Filters />
           </Col>
           <Col md={9}>
             <ProductList />
