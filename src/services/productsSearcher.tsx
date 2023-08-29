e9fb225ebbbc9e8f553cd103feba945b3a96c1d5
eordan <pistsouandrey@gmail.com>
2023-08-29 22:43:42 +0300

    feat: create sorting function

---
+++ src/services/productsSearcher.tsx
@@ -0,0 +1,53 @@
+import { createApiBuilderFromCtpClient, ProductProjectionPagedSearchResponse } from '@commercetools/platform-sdk';
+import { searchProductsCtpClient } from './withClientCredentialsFlowClientBuilder';
+import { ProductProjectionsQueryParameters } from '../utils/types';
+import { PROJECT_KEY } from './apiClientDetailsSetter';
+
+export const getQueryDetails = (
+  text: string | undefined = undefined,
+  filter: string | string[] | undefined = undefined,
+  sort: string | undefined = undefined,
+  fuzzy: boolean = true,
+  limit: number = 15,
+  offset: number = 0,
+) => {
+  return {
+    text,
+    fuzzy,
+    limit,
+    offset,
+    filter,
+    sort,
+  };
+};
+
+export const searchProducts = async (
+  params: ProductProjectionsQueryParameters,
+): Promise<ProductProjectionPagedSearchResponse> => {
+  const apiRoot = createApiBuilderFromCtpClient(searchProductsCtpClient).withProjectKey({
+    projectKey: PROJECT_KEY,
+  });
+
+  const data = apiRoot
+    .productProjections()
+    .search()
+    .get({
+      queryArgs: {
+        'text.en': params.text,
+        fuzzy: params.fuzzy,
+        limit: params.limit,
+        offset: params.offset,
+        filter: params.filter,
+        sort: params.sort,
+      },
+    })
+    .execute()
+    .then(({ body }) => {
+      return body;
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+  return data;
+};
