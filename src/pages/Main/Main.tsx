476e320f5756cb160d5fcc0f06fb188adee8a06c
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-09-02 21:48:02 +0300

    feat: rewrite success message with react-toastify

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
-import SuccessMessage from '@components/SuccessMessage';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer, toast, Slide } from 'react-toastify';
 import { Context } from '../../utils/createContext';
 
 import 'react-toastify/dist/ReactToastify.css';
@@ -12,26 +11,25 @@ export function Main(): JSX.Element {
   const user = useContext(Context);
 
   const notify = () => {
-    toast('Successfully logged in', {
+    toast.success('Successfully logged in', {
       position: toast.POSITION.TOP_CENTER,
       autoClose: 3000,
+      transition: Slide,
+      theme: 'colored',
+      className: 'success-message',
     });
   };
 
-  // useEffect(() => {
-  //   if (user.isEntered) {
-  //     notify();
-  //   }
-  // }, [user.isEntered]);
-
-  if (user.isEntered) {
-    useEffect(() => notify());
-  }
+  useEffect(() => {
+    if (user.isEntered) {
+      notify();
+      user.setIsEntered(false);
+    }
+  }, []);
 
   return (
     <section className="main">
       <Container className="d-flex main-container">
-        {user.isEntered && <SuccessMessage message="Successfully logged in" />}
         <h1 className="title">We love literature</h1>
         <ToastContainer />
       </Container>
