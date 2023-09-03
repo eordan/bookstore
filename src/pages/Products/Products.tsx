5ddaf00bda34f1a692df16e6bb28740c718b7976
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-09-03 18:18:05 +0300

    fix: delete unnecessary label

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,7 +1,7 @@
 import ProductList from '@components/ProductList';
 import Filters from '@containers/Filters';
 import React from 'react';
-import { Button, Col, Container, FloatingLabel, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
 export function Products(): JSX.Element {
   return (
@@ -10,13 +10,11 @@ export function Products(): JSX.Element {
         <Form className="d-flex justify-content-end pt-4">
           <Row>
             <Col>
-              <FloatingLabel label="Arrange">
-                <Form.Select>
-                  <option value="1">Cheap ones first</option>
-                  <option value="2">Expensive ones first</option>
-                  <option value="3">Alphabetically</option>
-                </Form.Select>
-              </FloatingLabel>
+              <Form.Select>
+                <option value="1">Cheap ones first</option>
+                <option value="2">Expensive ones first</option>
+                <option value="3">Alphabetically</option>
+              </Form.Select>
             </Col>
             <Col className="d-flex align-items-center">
               <Form.Control type="search" placeholder="Search" className="me-2" />
