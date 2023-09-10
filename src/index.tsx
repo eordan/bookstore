9e96807117c04d4db80cca2fb4885462b3d4a734
eordan <pistsouandrey@gmail.com>
2023-09-10 11:31:32 +0300

    feat: add 'getCart' function

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,11 @@ import 'react-bootstrap';
 import user from './utils/User';
 import store from './utils/Store';
 
-import { getQueryDetails, searchProducts } from './services/productsHandler/productsSearcher';
+// import { getQueryDetails, searchProducts } from './services/productsHandler/productsSearcher';
+
+import { getMyCartDraft, createAnonimousCart } from './services/ordersHandler/cartCreator';
+
+import { getCart } from './services/ordersHandler/cartGetter';
 
 import App from './components/App';
 import './index.scss';
@@ -17,6 +21,17 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   </Context.Provider>,
 );
 
-const details = getQueryDetails('Harry');
-const data = searchProducts(details);
-console.log(data);
+// const details = getQueryDetails('Harry');
+// const data = searchProducts(details);
+// console.log(data);
+
+const getCRT = async () => {
+  const draft = getMyCartDraft();
+  const cart = await createAnonimousCart(draft);
+  // console.log(cart);
+
+  const getcart = getCart(cart.id as string);
+  console.log(getcart);
+};
+
+getCRT();
--- src/services/ordersHandler/cartGetter.tsx
+++ src/services/ordersHandler/cartGetter.tsx
@@ -1,14 +1,14 @@
-import { Cart, MyCartDraft, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { Cart, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
 import { anonymousSessionFlowCtpClient } from '../flows/withAnonymousSessionFlowClientBuilder';
 import { PROJECT_KEY } from '../helpers/apiClientDetailsSetter';
 
-export const getMyCartDraft = () => {
-  return {
-    currency: 'CAD',
-  };
-};
+// export const getMyCartDraft = () => {
+//   return {
+//     currency: 'CAD',
+//   };
+// };
 
-export const createAnonimousCart = async (cartDetails: MyCartDraft): Promise<Cart> => {
+export const getCart = async (ID: string): Promise<Cart> => {
   const apiRoot = createApiBuilderFromCtpClient(anonymousSessionFlowCtpClient).withProjectKey({
     projectKey: PROJECT_KEY,
   });
@@ -16,9 +16,8 @@ export const createAnonimousCart = async (cartDetails: MyCartDraft): Promise<Car
   const data = apiRoot
     .me()
     .carts()
-    .post({
-      body: cartDetails,
-    })
+    .withId({ ID })
+    .get()
     .execute()
     .then(({ body }) => {
       return body;
