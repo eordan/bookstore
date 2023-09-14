cb03ad5fbc03089462547b90b9c031abb5d32781
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-09-14 20:10:57 +0300

    feat: add empty cart message

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,22 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Container, ListGroup, Form, Col } from 'react-bootstrap';
-// import { NavLink, Navigate, useNavigate } from 'react-router-dom';
-// import { RoutesEnum } from '../../utils/enums';
+import { Cart, LineItem } from '@commercetools/platform-sdk';
+import CartItem from '@components/CartItem';
+import { useNavigate } from 'react-router-dom';
+import { getCart } from '../../services/ordersHandler/cartGetter';
+import { RoutesEnum } from '../../utils/enums';
 
 import './Cart.scss';
 import '../../styles/main.scss';
-import { Cart, LineItem } from '@commercetools/platform-sdk';
-import CartItem from '@components/CartItem';
-import { getCart } from '../../services/ordersHandler/cartGetter';
+import emptyCart from '../../assets/empty-cart.svg';
 
 export function Basket(): JSX.Element {
+  const navigate = useNavigate();
   const [cart, setCart] = useState<Cart>();
+  const [isEmpty, setIsEmpty] = useState(true);
   let totalPrice = 0;
 
   useEffect(() => {
     if (localStorage.getItem('cartId')) {
-      getCart(`${localStorage.getItem('cartId')}`).then((data) => {
-        setCart(data);
+      getCart().then((data) => {
+        if (data.totalLineItemQuantity) {
+          setCart(data);
+          setIsEmpty(false);
+        }
       });
     }
   }, [localStorage.getItem('cartId')]);
@@ -26,29 +32,45 @@ export function Basket(): JSX.Element {
   }
 
   return (
-    <Container className="d-flex mt-3 cart gap-3">
-      <Col sm={12} md={9}>
-        <ListGroup className="w-100">
-          {cart?.lineItems.map((product: LineItem) => (
-            <CartItem key={product.id} product={product} />
-          ))}
-        </ListGroup>
-      </Col>
-      <Col sm={12} md={3} className="d-flex flex-column align-items-center p-0 mb-3">
-        <Form className="d-flex flex-column justify-content-between total w-100 p-3">
-          <Form.Text className="d-flex justify-content-between mb-4">
-            <h3>Total:</h3>
-            <h3>{totalPrice}$</h3>
-          </Form.Text>
-          <Form.Group className="mb-3">
-            <Form.Control placeholder="Have a promocode?" />
-          </Form.Group>
-          <Button type="submit">Place order</Button>
-        </Form>
-        <Button variant="outline-secondary" className="mt-4">
-          Clear cart
-        </Button>
-      </Col>
+    <Container>
+      {isEmpty && (
+        <Container className="d-flex flex-column justify-content-center align-items-center">
+          <img src={emptyCart} alt="empty cart" className="empty-cart" />
+          <h3>Your cart is empty</h3>
+          <p className="text-secondary text-center">
+            You have no items in your shopping cart.
+            <br />
+            Let&apos;s go buy something!
+          </p>
+          <Button onClick={() => navigate(RoutesEnum.PRODUCTS_ROUTE)}>Shop Now</Button>
+        </Container>
+      )}
+      {!isEmpty && (
+        <Container className="d-flex mt-3 cart gap-3">
+          <Col sm={12} md={9}>
+            <ListGroup className="w-100">
+              {cart?.lineItems.map((product: LineItem) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+            </ListGroup>
+          </Col>
+          <Col sm={12} md={3} className="d-flex flex-column align-items-center p-0 mb-3">
+            <Form className="d-flex flex-column justify-content-between total w-100 p-3">
+              <Form.Text className="d-flex justify-content-between mb-4">
+                <h3>Total:</h3>
+                <h3>{totalPrice}$</h3>
+              </Form.Text>
+              <Form.Group className="mb-3">
+                <Form.Control placeholder="Have a promocode?" />
+              </Form.Group>
+              <Button type="submit">Place order</Button>
+            </Form>
+            <Button variant="outline-secondary" className="mt-4">
+              Clear cart
+            </Button>
+          </Col>
+        </Container>
+      )}
     </Container>
   );
 }
