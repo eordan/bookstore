a041c2260ef42754fff9e73756ca5ac05ad08a47
Alina Radyukevich <lina.radyukevich98@gmail.com>
2023-09-18 23:54:14 +0300

    feat: implement the ability to increase/decrease products

--- src/components/ProductItem/ProductItem.tsx
+++ src/components/ProductItem/ProductItem.tsx
@@ -1,10 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
-import { ProductProjection } from '@commercetools/platform-sdk';
+import { observer } from 'mobx-react-lite';
+import { ProductProjection, Cart } from '@commercetools/platform-sdk';
 import { RoutesEnum } from '../../utils/enums';
 import { CATEGORIES } from '../../utils/constants';
-import { addLineItem, updateCart } from '../../services/ordersHandler/cartUpdater';
+import { addLineItem, removeLineItem, updateCart } from '../../services/ordersHandler/cartUpdater';
+import { getCart } from '../../services/ordersHandler/cartGetter';
 import { Context } from '../../utils/createContext';
 
 import './ProductItem.scss';
@@ -13,13 +15,16 @@ type ProductProps = {
   product: ProductProjection;
 };
 
-export function ProductItem({ product }: ProductProps): JSX.Element {
+export const ProductItem = observer(({ product }: ProductProps): JSX.Element => {
   const navigate = useNavigate();
   const { basket } = useContext(Context);
   let url = '';
   let category = '';
   let price = '';
   let author = '';
+  let productId: string = '';
+  const [isAdded, setIsAdded] = useState(false);
+  const [quantity, setQuantity] = useState(0);
 
   if (product.masterVariant.images) {
     const img = product.masterVariant.images[0];
@@ -40,12 +45,48 @@ export function ProductItem({ product }: ProductProps): JSX.Element {
     author = product.masterVariant.attributes[0].value;
   }
 
+  const cartControl = (data: Cart) => {
+    basket.setVersion(data.version);
+    if (data.totalLineItemQuantity) {
+      basket.setCount(data.totalLineItemQuantity);
+    } else {
+      basket.setCount(0);
+    }
+    const cartItem = data.lineItems.find((item) => item.productId === product.id);
+    if (cartItem) {
+      setQuantity(cartItem.quantity);
+    } else {
+      setIsAdded(false);
+      setQuantity(0);
+    }
+  };
+
+  useEffect(() => {
+    getCart().then((data) => {
+      const cartItem = data.lineItems.find((item) => item.productId === product.id);
+      productId = cartItem?.id as string;
+      if (cartItem) {
+        setIsAdded(true);
+        setQuantity(cartItem.quantity);
+      }
+    });
+  });
+
   const addToCart = () => {
     updateCart(basket.id, basket.version, [addLineItem(product.id)]).then((data) => {
-      basket.setVersion(data.version);
-      if (data.totalLineItemQuantity) {
-        basket.setCount(data.totalLineItemQuantity);
-      }
+      cartControl(data);
+    });
+  };
+
+  const increaseItems = () => {
+    updateCart(basket.id, basket.version, [addLineItem(product.id)]).then((data) => {
+      cartControl(data);
+    });
+  };
+
+  const decreaseItems = () => {
+    updateCart(basket.id, basket.version, [removeLineItem(productId)]).then((data) => {
+      cartControl(data);
     });
   };
 
@@ -67,16 +108,43 @@ export function ProductItem({ product }: ProductProps): JSX.Element {
           ) : (
             <Card.Text className="price">{price}</Card.Text>
           )}
-          <Button
-            onClick={(event) => {
-              event.stopPropagation();
-              addToCart();
-            }}
-          >
-            Add to cart
-          </Button>
+          {isAdded ? (
+            <div className="d-flex quantity-block">
+              <Button
+                variant="secondary"
+                className="quantity-item"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  decreaseItems();
+                }}
+              >
+                -
+              </Button>
+              <div className="quantity-item">{quantity}</div>
+              <Button
+                variant="secondary"
+                className="quantity-item"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  increaseItems();
+                }}
+              >
+                +
+              </Button>
+            </div>
+          ) : (
+            <Button
+              onClick={(event) => {
+                event.stopPropagation();
+                setIsAdded(true);
+                addToCart();
+              }}
+            >
+              Add to cart
+            </Button>
+          )}
         </Card.Body>
       </Card>
     </Col>
   );
-}
+});
